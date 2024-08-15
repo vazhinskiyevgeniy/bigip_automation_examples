12ad4750ae4e8bca29b552a96fc50c7c35ee485c
unknown <vazhinskiy.evgeniy@yoctocode.com>
2024-08-15 14:47:55 +0300

    csv14

--- bigip/bigip_next/security/migrate-from-tmos/migrate/generate_report.py
+++ bigip/bigip_next/security/migrate-from-tmos/migrate/generate_report.py
@@ -1,21 +1,32 @@
 import csv
 import sys
 
-# Function to generate a CSV report
-def generate_csv_report(output_file, data):
-    headers = ['hostname', 'ip_address']  # Update headers to match the keys in the data
+def generate_csv_report(output_file, data, deploy_tree, ip_map):
+    headers = ['hostname', 'ip_address', 'deploy_tree_key', 'deploy_tree_value', 'ip_map_key', 'ip_map_value']
     with open(output_file, 'w', newline='') as file:
         writer = csv.DictWriter(file, fieldnames=headers)
         writer.writeheader()
+
         for entry in data:
-            writer.writerow(entry)
+            for deploy_key, deploy_values in deploy_tree.items():
+                for deploy_value in deploy_values:
+                    for ip_key, ip_value in ip_map.items():
+                        row = {
+                            'hostname': entry.get('hostname'),
+                            'ip_address': entry.get('ip_address'),
+                            'deploy_tree_key': deploy_key,
+                            'deploy_tree_value': deploy_value['bigip_next'],
+                            'ip_map_key': ip_key,
+                            'ip_map_value': ip_value
+                        }
+                        writer.writerow(row)
 
 if __name__ == "__main__":
-    # Assuming that data is passed as a JSON-encoded string in the first argument
-    # Ansible can pass data to this script in JSON format
     import json
 
-    data = json.loads(sys.argv[1])
-    output_file = sys.argv[2]
+    output_file = sys.argv[1]
+    data = json.loads(sys.argv[2])
+    deploy_tree = json.loads(sys.argv[3])
+    ip_map = json.loads(sys.argv[4])
 
-    generate_csv_report(output_file, data)
+    generate_csv_report(output_file, data, deploy_tree, ip_map)
