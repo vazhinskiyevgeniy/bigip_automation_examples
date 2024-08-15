f58563178493187b86604271c0152c36d6e0d442
unknown <vazhinskiy.evgeniy@yoctocode.com>
2024-08-15 18:20:34 +0300

    csv30

--- bigip/bigip_next/security/migrate-from-tmos/migrate/generate_report.py
+++ bigip/bigip_next/security/migrate-from-tmos/migrate/generate_report.py
@@ -1,34 +1,27 @@
 import csv
-import json
 import sys
+import json
 
-def generate_csv_report(output_file, deploy_tree, ip_map):
-    headers = ['deploy_tree_key', 'deploy_tree_value', 'ip_map_key', 'ip_map_value']
+def generate_csv_report(output_file, migrate_apps, migrate_app_prefix):
+    headers = ['Old_App_Name', 'New_App_Name', 'Status', 'Old_IP_Address']
     with open(output_file, 'w', newline='') as file:
         writer = csv.DictWriter(file, fieldnames=headers)
         writer.writeheader()
 
-        for deploy_key, deploy_values in deploy_tree.items():
-            for deploy_value in deploy_values:
-                for ip_key, ip_value in ip_map.items():
-                    row = {
-                        'deploy_tree_key': deploy_key,
-                        'deploy_tree_value': deploy_value['bigip_next'],
-                        'ip_map_key': ip_key,
-                        'ip_map_value': ip_value
-                    }
-                    writer.writerow(row)
-
-# def save_cm_apps_json(cm_apps, output_json_file):
-#     with open(output_json_file, 'w') as json_file:
-#         json.dump(cm_apps, json_file, indent=4)
+        for app in migrate_apps:
+            for vs in app['virtual_servers']:
+                app_name = vs['name']
+                row = {
+                    'App_Name_Cut': app_name.replace(migrate_app_prefix, '', 1),
+                    'App_Name': app_name,
+                    'Status': vs['status'],
+                    'IP_Address': vs['ip_addresses'][0] if vs['ip_addresses'] else ''
+                }
+                writer.writerow(row)
 
 if __name__ == "__main__":
     output_file = sys.argv[1]
-    deploy_tree = json.loads(sys.argv[2])
-    ip_map = json.loads(sys.argv[3])
-    # cm_apps = json.loads(sys.argv[4]) 
-    # output_json_file = sys.argv[5]
+    migrate_apps = json.loads(sys.argv[2])
+    migrate_app_prefix = sys.argv[3]
 
-    generate_csv_report(output_file, deploy_tree, ip_map)
-    # save_cm_apps_json(cm_apps, output_json_file)
+    generate_csv_report(output_file, migrate_apps, migrate_app_prefix)
